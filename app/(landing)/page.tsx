6c476d4629f85a82d4c19347322b53e03ee6613d
Navpreet singh <navpreetsingh41548@gmail.com>
2024-04-14 12:02:54 +0530

    Added Image Background

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,30 +1,36 @@
 "use client";
-import { ArrowRight, Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+import {
+  ArrowRight,
+  Code,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  VideoIcon,
+} from "lucide-react";
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import ConversationPage from "@/app/(dashboard)/(routes)/conversation/page";
-
+import styles from "./page.module.css";
+import Image from "next/image";
 
 const tools = [
   {
-    label:"Coversation",
+    label: "Coversation",
     icon: MessageSquare,
     color: "text-violet-500",
     bgColor: "bg-violet-500/10",
-    href: "/conversation"
+    href: "/conversation",
   },
-]
+];
 
-
-const DashboardPage = ()  => {
+const DashboardPage = () => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://assets.calendly.com/assets/external/widget.js';
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
     script.async = true;
-
 
     document.head.appendChild(script);
 
@@ -38,23 +44,35 @@ const DashboardPage = ()  => {
 
   const showConversation = () => {
     setConversationVisible(true);
-}
+  };
 
-
-const router = useRouter();  
+  const router = useRouter();
   return (
     <div>
-       <div>
-                <Button className="circular-button" onClick={showConversation}>
-                    Click Me  
-                </Button> 
-           {/* this is my button*/}
-        </div>
+      <div className={styles.container}>
+        <Image
+          className={styles.backgroundImage}
+          src={"/imgBackground.jpg"}
+          width={1920}
+          height={941}
+          alt="Dashboard Image"
+        />
+        <Button
+          className={`circular-button ${styles.circularButton}`}
+          onClick={showConversation}
+        >
+          Click Me
+        </Button>
+        {/* this is my button*/}
+      </div>
       {/* Include the Calendly badge widget */}
-      <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet" />
+      <link
+        href="https://assets.calendly.com/assets/external/widget.css"
+        rel="stylesheet"
+      />
 
       {ConversationVisible && <ConversationPage />}
     </div>
-  )
-}
+  );
+};
 export default DashboardPage;
